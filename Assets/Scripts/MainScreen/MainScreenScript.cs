47ef172e95500fee997a897e798f1ec41de7ef6e
Jiayu Zhang <48386227+JavuesZhang@users.noreply.github.com>
2022-01-22 19:17:13 +0800

    main screen & up/down button

---
+++ Assets/Scripts/MainScreen/MainScreenScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using LitJson;
+using System.IO;
+public class Images {
+    public string path { get; set; }
+    public int x { get; set; }
+    public int y { get; set; }
+}
+
+public class Texts {
+    public string path { get; set; }
+    public int x { get; set; }
+    public int y { get; set; }
+}
+
+public class SinglePage {
+    public string title { get; set; }
+    public List<Images> images { get; set; }
+    public List<Texts> texts { get; set; }
+}
+public class Pages {
+    public List<SinglePage> pages { get; set; }
+}
+public class MainScreenScript : MonoBehaviour
+{
+    private Pages obj;
+    private int curPage = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = ReadJson("/Data/MainScreenData.json");
+    }
+
+    public void loadScreen(int index) {
+        Debug.Log(obj.pages[index].title);
+        transform.GetComponentInChildren<Text>().text = obj.pages[index].title;
+    }
+
+    private Pages ReadJson(string str)
+    {
+        StreamReader StreamReader = new StreamReader(Application.dataPath + str);
+        JsonReader js = new JsonReader(StreamReader);
+        return JsonMapper.ToObject<Pages>(js);
+    }
+
+    public void onClickPageUp() {
+        curPage = (curPage + 1 + obj.pages.Count) % obj.pages.Count;
+        loadScreen(curPage);
+    }
+    public void onClickPageDown() {
+        curPage = (curPage - 1 + obj.pages.Count) % obj.pages.Count;
+        loadScreen(curPage);
+    }
+}
---
+++ Assets/Scripts/Utility/AssetUtil.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Data;
+using UnityEditor;
+public class AssetUtil
+{
+    private static string assetFolderPath = "Assets/Resources/";
+
+    public static void CreateAsset(string assetName, Object assetData)
+    {
+        //确保文件夹存在
+        if (!Directory.Exists(assetFolderPath))
+        {
+            Directory.CreateDirectory(assetFolderPath);
+        }
+
+        //asset文件的路径 要以"Assets/..."开始，否则CreateAsset会报错
+        string assetPath = string.Format("{0}{1}.asset", assetFolderPath, assetName);
+        //生成一个Asset文件
+        AssetDatabase.CreateAsset(assetData, assetPath);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+}
