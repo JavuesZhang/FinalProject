b8184582035e451283210e7e645bb7064e4bb20b
Jiayu Zhang <48386227+JavuesZhang@users.noreply.github.com>
2022-01-23 20:51:49 +0800

    add image & text

--- Assets/Scripts/MainScreen/MainScreenScript.cs
+++ Assets/Scripts/MainScreen/MainScreenScript.cs
@@ -6,14 +6,18 @@ using LitJson;
 using System.IO;
 public class Images {
     public string path { get; set; }
-    public int x { get; set; }
-    public int y { get; set; }
+    public int x { get; set; } = -150;
+    public int y { get; set; } = 0;
+    public int width { get; set; } = 300;
+    public int height { get; set; } = 400;
 }
 
 public class Texts {
     public string path { get; set; }
-    public int x { get; set; }
-    public int y { get; set; }
+    public int x { get; set; } = 150;
+    public int y { get; set; } = 0;
+    public int width { get; set; } = 300;
+    public int height { get; set; } = 400;
 }
 
 public class SinglePage {
@@ -26,24 +30,69 @@ public class Pages {
 }
 public class MainScreenScript : MonoBehaviour
 {
+    public GameObject txtTemplate;
+    public GameObject imgTemplate;
+
     private Pages obj;
     private int curPage = 0;
+    private List<GameObject> txts;
+    private List<GameObject> imgs;
     // Start is called before the first frame update
     void Start()
     {
         obj = ReadJson("/Data/MainScreenData.json");
+        txts = new List<GameObject>();
+        imgs = new List<GameObject>();
     }
 
     public void loadScreen(int index) {
-        Debug.Log(obj.pages[index].title);
-        transform.GetComponentInChildren<Text>().text = obj.pages[index].title;
+        foreach(var item in txts) {
+            Destroy(item);
+        }
+        foreach(var item in imgs) {
+            Destroy(item);
+        }
+        foreach(var item in GetComponentsInChildren<Text>()) {
+            if(item.name == "Title") {
+                item.text = obj.pages[index].title;
+            }
+        }
+        foreach(var text in obj.pages[index].texts) {
+            GameObject tmpTxt = Instantiate(txtTemplate, transform) as GameObject;
+            tmpTxt.GetComponent<Text>().text = ReadText(text.path);
+            tmpTxt.GetComponent<RectTransform>().anchoredPosition = new Vector2(text.x, text.y);
+            tmpTxt.GetComponent<RectTransform>().sizeDelta = new Vector2(text.width, text.height);
+            txts.Add(tmpTxt);
+        }
+        foreach(var image in obj.pages[index].images) {
+            GameObject tmpImg = Instantiate(imgTemplate, transform) as GameObject;
+            tmpImg.GetComponent<Image>().sprite = ReadImage(image.path);
+            tmpImg.GetComponent<RectTransform>().anchoredPosition = new Vector2(image.x, image.y);
+            tmpImg.GetComponent<RectTransform>().sizeDelta = new Vector2(image.width, image.height);
+            imgs.Add(tmpImg);
+        }
     }
 
-    private Pages ReadJson(string str)
-    {
+    private Pages ReadJson(string str) {
         StreamReader StreamReader = new StreamReader(Application.dataPath + str);
         JsonReader js = new JsonReader(StreamReader);
         return JsonMapper.ToObject<Pages>(js);
+    }
+
+    private string ReadText(string str) {
+        string s = "";
+        StreamReader StreamReader = new StreamReader(Application.dataPath + str);
+        string line;
+        while((line = StreamReader.ReadLine()) != null) {
+            s += line + '\n';
+        }
+        return s;
+    }
+
+    private Sprite ReadImage(string str) {
+        Sprite sp = Resources.Load(str, typeof(Sprite)) as Sprite;
+        Debug.Log(sp);
+        return sp;
     }
 
     public void onClickPageUp() {
