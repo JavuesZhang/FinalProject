26fb71f9a1dce06c9e8b7804289daf2ca20ca3a1
SmireG <402809775@qq.com>
2022-02-13 03:37:25 +0800

    update operation step

--- Assets/Scripts/MainScreen/OperationStepsScript.cs
+++ Assets/Scripts/MainScreen/OperationStepsScript.cs
@@ -19,6 +19,8 @@ public class OperationPages {
 }
 public class OperationStepsScript : MonoBehaviour
 {
+    public GameObject UpBtn;
+    public GameObject DownBtn;
     public GameObject Step;
     public GameObject Content;
     private OperationPages obj;
@@ -29,11 +31,18 @@ public class OperationStepsScript : MonoBehaviour
     private int doneCount = 0;
 
     private int maxPageIndex;
+
+    private Color DarkGray = new Color(255, 0, 0);
+    private Color Black = new Color(0, 0, 0);
     // Start is called before the first frame update
     void Start() {
         obj = ReadJson("/Data/OperationStepsData.json");
         maxPageIndex = obj.pages.Count;
         steps = new List<GameObject>();
+        if(maxPageIndex > 0) {
+            loadScreen(0);
+        }
+        checkInteractable();
     }
 
     private OperationPages ReadJson(string str) {
@@ -50,21 +59,17 @@ public class OperationStepsScript : MonoBehaviour
         doneCount = 0;
 
         // load new steps
-        for (int i = 0; i < obj.pages[index].steps.Count;i++) {
+        for (int i = 0; i < obj.pages[index].steps.Count; i++) {
             GameObject tmpStep = Instantiate(Step, Content.transform) as GameObject;
             tmpStep.GetComponentInChildren<Text>().text = (i+1).ToString() + ". " + obj.pages[index].steps[i].name;
-            if(obj.pages[index].steps[i].done) {
-                doneCount++;
-            } else {
-                var tmpTxtColor = tmpStep.GetComponentInChildren<Text>().color;
-                tmpStep.GetComponentInChildren<Image>().color = new Color(tmpTxtColor.r, tmpTxtColor.g, tmpTxtColor.b,(float)0.5);
-                var tmpImgColor = tmpStep.GetComponentInChildren<Image>().color;
-                tmpStep.GetComponentInChildren<Image>().color = new Color(tmpImgColor.r, tmpImgColor.g, tmpImgColor.b,0);
-            }
+            Debug.Log(obj.pages[index].steps[i].name);
+            checkStepState(tmpStep, obj.pages[index].steps[i].done);
+
             tmpStep.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -30 - (50) * i);
             steps.Add(tmpStep);
         }
 
+        Content.GetComponent<RectTransform>().sizeDelta = new Vector2(Content.GetComponent<RectTransform>().sizeDelta.x, 50 * obj.pages[index].steps.Count);
         foreach(var item in GetComponentsInChildren<Text>()) {
             if(item.name == "Title") {
                 item.text = obj.pages[index].title;
@@ -78,12 +83,39 @@ public class OperationStepsScript : MonoBehaviour
         }
     }
 
+    private void checkStepState(GameObject step, bool done) {
+        var imgColor = step.GetComponentInChildren<Image>().color;
+        if(done) {
+            doneCount++;
+            // 颜色设置为灰色，隐藏√
+            step.GetComponentInChildren<Text>().color = DarkGray;
+            step.GetComponentInChildren<Image>().color = new Color(imgColor.r, imgColor.g, imgColor.b,0);
+        } else {
+            // 颜色设置为黑色，显示√
+            step.GetComponentInChildren<Text>().color = Black;
+            step.GetComponentInChildren<Image>().color = new Color(imgColor.r, imgColor.g, imgColor.b,255);
+        }
+    }
+    void checkInteractable() {
+        if(curPage > 0) {
+            UpBtn.GetComponent<Button>().interactable = true;
+        } else {
+            UpBtn.GetComponent<Button>().interactable = false;
+        }
+        if(curPage < maxPageIndex - 1) {
+            DownBtn.GetComponent<Button>().interactable = true;
+        } else {
+            DownBtn.GetComponent<Button>().interactable = false;
+        }
+    }
     public void onClickPageUp() {
         curPage = (curPage + 1 + maxPageIndex) % maxPageIndex;
         loadScreen(curPage);
+        checkInteractable();
     }
     public void onClickPageDown() {
         curPage = (curPage - 1 + maxPageIndex) % maxPageIndex;
         loadScreen(curPage);
+        checkInteractable();
     }
 }
